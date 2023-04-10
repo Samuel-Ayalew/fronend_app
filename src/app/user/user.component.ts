ec016eaf901e9a2860f815a7af1826b25a039185
samuel-ayalew <samuelayalew.dventus@gmail.com>
2023-04-10 11:49:42 +0300

    creat the html page modified the user.ts data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import {User} from '../user';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+ users: User[] | undefined;
+  constructor(private userService:UserService){ }
+
+
+  ngOnInit(): void {
+      this.userService.getUsers().subscribe((data: User[]) => {
+        console.log(data);
+        this.users = data;
+      });
+  }
 
 }
